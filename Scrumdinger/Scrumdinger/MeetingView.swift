0d23f60ecf0f0e73ca753285d05780cafd4f14b5
Jacob Willis <willisjp@dukes.jmu.edu>
2022-03-05 09:32:33 -0500

    Update

---
+++ Scrumdinger/Scrumdinger/MeetingView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by Jacob Willis on 3/5/22.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct MeetingView_Preview: PreviewProvider {
+    static var previews: some View {
+        MeetingView()
+    }
+}
---
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Jacob Willis on 3/5/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
