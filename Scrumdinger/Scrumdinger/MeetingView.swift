a2422598cc89a45b74d46c0edfcc743ef53ba20e
Jacob Willis <willisjp@dukes.jmu.edu>
2022-03-07 08:44:14 -0500

    Update MeetingView.swift

--- Scrumdinger/Scrumdinger/MeetingView.swift
+++ Scrumdinger/Scrumdinger/MeetingView.swift
@@ -9,8 +9,33 @@ import SwiftUI
 
 struct MeetingView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            ProgressView( value: 5, total: 15 )
+            HStack {
+                VStack (alignment: .leading){
+                    Text("Second Elapsed").font(.caption)
+                    Label("300", systemImage:"hourglass.bottomhalf.fill")
+                }
+                Spacer()
+                VStack (alignment: .trailing){
+                    Text("Seconds Remaining").font(.caption)
+                    Label("600", systemImage:"hourglass.tophalf.fill")
+                }
+            }
+            .accessibilityElement(children: .ignore)
+            .accessibilityLabel("Time Remaining")
+            .accessibilityValue("10 minutes")
+            Circle().strokeBorder(lineWidth: 24)
+            HStack {
+                Text("Speaker 1 of 3")
+                Spacer()
+                Button(action: {}) {
+                    Image(systemName: "forward.fill")
+                }
+                .accessibilityLabel("Next Speaker")
+            }
+        }
+        .padding()
     }
 }
 
