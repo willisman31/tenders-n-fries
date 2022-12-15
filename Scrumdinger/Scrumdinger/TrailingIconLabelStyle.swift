0ba0ce4e66c2b6357191a31594b728662a9ae124
Jacob Willis <willisjp@dukes.jmu.edu>
2022-12-14 19:56:46 -0500

    Update

---
+++ Scrumdinger/Scrumdinger/TrailingIconLabelStyle.swift
@@ -0,0 +1,22 @@
+//
+//  TrailingIconLabelStyle.swift
+//  Scrumdinger
+//
+//  Created by Jacob Willis on 12/14/22.
+//
+
+import Foundation
+import SwiftUI
+
+struct TrailingIconLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title
+            configuration.icon
+        }
+    }
+}
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self { Self() }
+}
